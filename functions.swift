cdb57d5c61995e1d5c809a2c7c5037184eed3a8f
Sam Galizia <samgalizia@gmail.com>
2017-09-07 17:35:23 -0700

    Added functions.swift

---
+++ functions.swift
@@ -0,0 +1,17 @@
+// Fibonacci Problem
+func fibonacci(numToPrint: Int) {
+
+}
+
+// Factorial Recursive
+// I understand the concept, but I am having trouble actually forming the
+// algorithm in my head to make this work :/ I do know what they are and
+// how they work, jsut can't seem to make them myself.
+func factorial_recursive(num: Int) {
+  if num == 1 {
+
+  }
+  else {
+    factorial_recursive(num: num - 1)
+  }
+}
